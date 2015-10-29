1ee6baaf1711444839a04d7b17a244ee59920499
amir5000 <amirarafat1@gmail.com>
2015-10-29 16:54:07 -0600

    got rid of onPan function and used the onPanEnd event for better handleing of how many times the function used to be called.

--- src/jsx/tabsOuter.jsx
+++ src/jsx/tabsOuter.jsx
@@ -14,39 +14,21 @@ module.exports = React.createClass({
 	handleSelect: function(key) {
 		this.setState({key: key});
 	},
-	handlePan: function(event) {
-		var threshold = 10;
-		if (this.state.fire === 1) {
-			if (event.deltaX < -threshold && this.refs.TabsRef.props.children.length > this.state.key) {
-				this.setState({key: this.state.key+1});
-			} else if (event.deltaX < -threshold && this.refs.TabsRef.props.children.length === this.state.key) {
-				this.setState({
-					key: 1
-				});
-			} else if (event.deltaX > threshold && this.state.key !== 1) {
-				this.setState({key: this.state.key-1});
-			} else if (event.deltaX > threshold && this.state.key === 1) {
-				var childrenLength = this.refs.TabsRef.props.children.length;
-				this.setState({
-					key: childrenLength
-				});
-			}
-			this.setState({
-				fire: 0
-			});
-		}
-	},
 	handlePanEnd: function() {
-		setTimeout( function() { 
-			this.setState({
-				fire: 1
-			});
-		}.bind(this), 1000); // needed this to avoid weird things from hapening if you swipe really fast multiple time in a row.
-		
+			if (this.refs.TabsRef.props.children.length > this.state.key) {
+				this.setState({key: this.state.key+1});
+			} else if (this.refs.TabsRef.props.children.length === this.state.key) {
+				this.setState({key: 1});
+			} else if (this.state.key !== 1) {
+				this.setState({key: this.state.key-1});
+			} else if (this.state.key === 1) {
+				var childrenLength = this.refs.TabsRef.props.children.length;
+				this.setState({key: childrenLength});
+			}
 	},
   	render: function() {
 	    return (
-	    	<Hammer onPanEnd={this.handlePanEnd} onPan={this.handlePan}>
+	    	<Hammer onPanEnd={this.handlePanEnd}>
 			    <Tabs justified ref="TabsRef" defaultActiveKey={1} activeKey={this.state.key} onSelect={this.handleSelect}>
 					    <Tab eventKey={1} title="Tab 1">
 						    <h3>Tab 1 Header</h3>
