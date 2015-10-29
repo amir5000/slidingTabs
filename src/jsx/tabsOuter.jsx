3fff0922e2127319cf37233a4d544b8ff0c8b109
amir5000 <amirarafat1@gmail.com>
2015-10-29 17:15:24 -0600

    added back pan for directions only instead of bhandling the change of active tab

--- src/jsx/tabsOuter.jsx
+++ src/jsx/tabsOuter.jsx
@@ -8,27 +8,52 @@ module.exports = React.createClass({
 	getInitialState: function() {
 		return {
 			key: 1,
+			direction: "",
 			fire: 1
 		};
 	},
 	handleSelect: function(key) {
 		this.setState({key: key});
 	},
+	handlePan: function(event) {
+		if (this.state.fire === 1) {
+			if (event.deltaX > 10) {
+				this.setState({
+					direction: "left"
+				});
+			} else if (event.deltaX < -10) {
+				this.setState({
+					direction: "right"
+				});
+			}
+
+			this.setState({
+				fire: 0
+			});
+		}
+	},
 	handlePanEnd: function() {
+		if (this.state.direction === "right") {
 			if (this.refs.TabsRef.props.children.length > this.state.key) {
 				this.setState({key: this.state.key+1});
 			} else if (this.refs.TabsRef.props.children.length === this.state.key) {
 				this.setState({key: 1});
-			} else if (this.state.key !== 1) {
+			} 
+		} else if (this.state.direction === "left") {
+			if (this.state.key !== 1) {
 				this.setState({key: this.state.key-1});
 			} else if (this.state.key === 1) {
 				var childrenLength = this.refs.TabsRef.props.children.length;
 				this.setState({key: childrenLength});
 			}
+		}
+		this.setState({
+			fire: 1
+		});
 	},
   	render: function() {
 	    return (
-	    	<Hammer onPanEnd={this.handlePanEnd}>
+	    	<Hammer onPanEnd={this.handlePanEnd} onPan={this.handlePan}>
 			    <Tabs justified ref="TabsRef" defaultActiveKey={1} activeKey={this.state.key} onSelect={this.handleSelect}>
 					    <Tab eventKey={1} title="Tab 1">
 						    <h3>Tab 1 Header</h3>
